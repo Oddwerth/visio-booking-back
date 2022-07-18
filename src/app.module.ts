529ef19d3daae7da0b4eadb7c202df011aee92f3
Adrien GEORGEON <adrien.georgeon@protonmail.com>
2022-07-18 23:27:34 +0200

    [EVO] - User schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from "@nestjs/config";
 import {MongooseModule} from "@nestjs/mongoose";
+import { UserModule } from './user/user.module';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.DB_URL)
+    MongooseModule.forRoot(process.env.DB_URL),
+    UserModule
   ],
   controllers: [],
   providers: [],
---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,54 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop()
+  userName: string
+
+  @Prop()
+  name: string
+
+  @Prop()
+  firstName: string
+
+  @Prop()
+  contactMail: string
+
+  @Prop()
+  platform: string[]
+
+  @Prop()
+  alias: string
+
+  @Prop()
+  description1: string
+
+  @Prop()
+  description2: string
+
+  @Prop()
+  description3: string
+
+  @Prop()
+  photo: string[]
+
+  @Prop()
+  language: string[]
+
+  @Prop()
+  country: string
+
+  @Prop()
+  manager: string
+
+  @Prop()
+  ManagerEmail: string
+
+  @Prop()
+  tags: string[]
+}
+
+export const UserSchema = SchemaFactory.createForClass(User)
